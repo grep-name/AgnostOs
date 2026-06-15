96c2c2308305c39dfe23fef248703c0c0ec0bc10
guru901 <avinashkaur842@gmail.com>
2026-06-15 18:14:14 +0530

    add bios to repo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cargo build --release
 mkdir -p esp/EFI/BOOT
 cp target/x86_64-unknown-uefi/release/something.efi esp/EFI/BOOT/BOOTX64.EFI
-qemu-system-x86_64 -bios /usr/share/edk2/x64/OVMF.4m.fd -drive format=raw,file=fat:rw:esp
+qemu-system-x86_64 -bios ./bios/OVMF.4m.fd -drive format=raw,file=fat:rw:esp
