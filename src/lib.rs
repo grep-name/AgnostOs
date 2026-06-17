25034ab4c1d834bb99e8890f6ec28d7fc91c9d79
guru901 <avinashkaur842@gmail.com>
2026-06-17 20:14:13 +0530

    add comments to lib.rs for different modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+/// Module that contains the code for our custom allocator.
 pub mod allocator;
+
+/// Module that contains the code for rendering things to the screen after exiting uefi boot
+/// services. It usses framebuffer to write the bytes directly
 pub mod graphics;
+
+/// Module that contains the code for rendering things to the screen when in uefi. It usses gop.
 pub mod uefi_graphics;
